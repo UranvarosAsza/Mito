92fbe6a5a62977dde05da003f6ebf93264ce7dff
haldat <haldat@dexter.hu>
2024-02-20 18:34:09 +0100

    routing and base views added

--- Airline/src/App.vue
+++ Airline/src/App.vue
@@ -13,6 +13,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink :to="{name: 'Home' }">Home</RouterLink>
+        <RouterLink :to="{name: 'SelectFlight' }">Select Flight</RouterLink>
       </nav>
     </div>
   </header>
--- Airline/src/router/index.ts
+++ Airline/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
+import SelectFlight from '@/views/SelectFlight.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/selectflight',
+      name: 'SelectFlight',
+      component: SelectFlight
     },
     {
       path: '/about',
